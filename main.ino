68b678a16ca221a7921af843dcbfe9a6ffac38c5
Hoga Cavan Afrinata <91598145+heronhoga@users.noreply.github.com>
2023-12-13 15:31:20 +0700

    feat:add gps code

--- main.ino
+++ main.ino
@@ -1,30 +1,96 @@
-
+//GPS
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+//LCD
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27,16,2);
 
-#include <OneWire.h>
-#include <DallasTemperature.h>
-#define ONE_WIRE_BUS 8
+int RXPin = 2;
+int TXPin = 3;
 
-OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature sensors(&oneWire);
+int GPSBaud = 9600;
+TinyGPSPlus gps;
+SoftwareSerial gpsSerial(RXPin, TXPin);
 
-float celc = 0;
 
-LiquidCrystal_I2C lcd(0x27,20,4);
-
-void setup() {
+void setup()
+{
   Serial.begin(9600);
+  gpsSerial.begin(GPSBaud);
   lcd.init();
+  // Print a message to the LCD.
   lcd.backlight();
 }
 
-void loop() {
-  sensors.requestTemperatures();
-  celc = sensors.getTempCByIndex(0);
-  lcd.setCursor(0,0);
-  lcd.print("Wazzup Hoga!");
-  lcd.setCursor(0, 1);
-  lcd.print("Suhu: " + String(celc));
+void loop()
+{
+  while (gpsSerial.available() > 0)
+    if (gps.encode(gpsSerial.read()))
+      displayInfo();
+
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+  {
+    Serial.println("No GPS detected");
+    while(true);
+  }
+}
+
+void displayInfo()
+{
+  if (gps.location.isValid())
+  {
+    Serial.print("Latitude: ");
+    Serial.println(gps.location.lat(), 6);
+    Serial.print("Longitude: ");
+    Serial.println(gps.location.lng(), 6);
+    Serial.print("Altitude: ");
+    Serial.println(gps.altitude.meters());
+    lcd.setCursor(0, 0);
+    lcd.print("Wazzup dice!");
+    lcd.setCursor(0, 1);
+    lcd.print("Posisi ditemukan");
+  }
+  else
+  {
+    Serial.println("Location: Not Available");
+  }
+  
+  Serial.print("Date: ");
+  if (gps.date.isValid())
+  {
+    Serial.print(gps.date.month());
+    Serial.print("/");
+    Serial.print(gps.date.day());
+    Serial.print("/");
+    Serial.println(gps.date.year());
+  }
+  else
+  {
+    Serial.println("Not Available");
+  }
+
+  Serial.print("Time: ");
+  if (gps.time.isValid())
+  {
+    if (gps.time.hour() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.hour());
+    Serial.print(":");
+    if (gps.time.minute() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.minute());
+    Serial.print(":");
+    if (gps.time.second() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.second());
+    Serial.print(".");
+    if (gps.time.centisecond() < 10) Serial.print(F("0"));
+    Serial.println(gps.time.centisecond());
+  }
+  else
+  {
+    Serial.println("Not Available");
+  }
+
+  Serial.println();
+  Serial.println();
   delay(1000);
 }
