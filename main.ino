93351a1a3e5332c01f48017d62a2434797847795
Hoga Cavan Afrinata <91598145+heronhoga@users.noreply.github.com>
2024-01-04 12:09:51 +0700

    feat:add temperature sensor (DS18B20)

--- main.ino
+++ main.ino
@@ -1,10 +1,19 @@
 //GPS
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
+
 //LCD
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27,16,2);
+
+//TEMPERATURE
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#define ONE_WIRE_BUS 8
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
 
 int RXPin = 2;
 int TXPin = 3;
@@ -17,10 +26,15 @@ SoftwareSerial gpsSerial(RXPin, TXPin);
 void setup()
 {
   Serial.begin(9600);
+  //GPS
   gpsSerial.begin(GPSBaud);
+
+  //LCD
   lcd.init();
-  // Print a message to the LCD.
   lcd.backlight();
+
+  //TEMPERATURE
+  sensors.begin();
 }
 
 void loop()
@@ -37,8 +51,28 @@ void loop()
   }
 }
 
+
 void displayInfo()
 {
+  lcd.setCursor(0, 0);
+  lcd.print("DICE-AQ-1");
+
+  sensors.requestTemperatures();
+  float tempC = sensors.getTempCByIndex(0);
+
+  // Check if reading was successful
+  if(tempC != DEVICE_DISCONNECTED_C) 
+  {
+
+    lcd.setCursor(0, 1);
+    lcd.print(tempC);
+  } 
+  else
+  {
+    lcd.setCursor(0, 1);
+    lcd.print("Err read temp");
+  }
+
   if (gps.location.isValid())
   {
     Serial.print("Latitude: ");
@@ -52,11 +86,6 @@ void displayInfo()
     Serial.print(gps.location.lat(), 6); // Print latitude with 6 decimal places
     Serial.print("%2C");
     Serial.println(gps.location.lng(), 6);
-
-    lcd.setCursor(0, 0);
-    lcd.print("Wazzup dice!");
-    lcd.setCursor(0, 1);
-    lcd.print("Posisi ditemukan");
   }
   else
   {
